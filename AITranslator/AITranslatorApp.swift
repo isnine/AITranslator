bc3dba5309e1dd9e09252e0f3bbad7e30cc6ac74
Zander Wang <xiaozwan@microsoft.com>
2025-10-18 11:03:00 +0800

    Initial Commit

---
+++ AITranslator/AITranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITranslatorApp.swift
+//  AITranslator
+//
+//  Created by Zander Wang on 2025/10/18.
+//
+
+import SwiftUI
+
+@main
+struct AITranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
